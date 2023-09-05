50ada2b02af77a5c1ffb079a3ce410e209387b1e
Alexandre Bueno <86690935+buenoalexandre@users.noreply.github.com>
2023-09-05 20:22:38 -0300

    primeira versão do site em laravel

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index' ]);
+Route::get('/events/create', [EventController::class, 'create' ]);
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
+
+
+Route::get('/produtos_teste/{id}', function ( $id) {
+    return view('product', ['id' => $id]);
 });
